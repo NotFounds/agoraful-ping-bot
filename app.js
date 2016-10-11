688469c74caff665ff496d6183895cf14ceb1c11
Ikeda Iori <m134_warrock_rpg7@yahoo.com>
2016-10-11 23:29:10 +0900

    Add some replys

--- app.js
+++ app.js
@@ -17,17 +17,43 @@ app.use(function *(){
   // var thread = payload.thread;
   // var user = payload.user;
 
-  if (isPOST && isJSON && event == 'post' && post.text == 'ping') {
-    console.log('pong');
-    this.body = {
-      actions: [{
-        action: 'post',
-        payload: {
-          title: '',
-          text: 'pong'
+  if (isPOST && isJSON && event == 'post') {
+    var doResponse = true;
+    var title = '';
+    var text = '';
+    switch (post.text) {
+      case 'Hello':
+        text = 'World!';
+        break;
+      case 'ねこ':
+        text = 'にゃー';
+        break;
+      case 'おはよう':
+        text = 'おはよう！';
+        break;
+      case 'サイコロ':
+        text =  String(Math.floor( Math.random() * 5 ) + 1);
+        break;
+      default:
+        var index = post.text.indexOf('した.');
+        if (index != -1) {
+          text = post.text.slice(0, index) + 'したなんてすごいですね!!';
+        } else {
+          doResponse = false;
         }
-      }]
-    };
+        break;
+    }
+    if (doResponse) {
+      this.body = {
+        actions: [{
+          action: 'post',
+          payload: {
+            title: title,
+            text: text,
+          }
+        }]
+      };
+    }
   }
 });
 
