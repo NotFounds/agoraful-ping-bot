de62bba6100c649733e86314140c6993a38f6013
NotFounds <m134_warrock_rpg7@yahoo.com>
2016-10-12 12:44:13 +0900

    Add some reply

--- app.js
+++ app.js
@@ -31,17 +31,53 @@ app.use(function *(){
         text = 'にゃー';
         break;
       case 'おはよう':
-        text = 'おはよう！';
+        text = 'おはようなぎ〜';
         break;
+      case 'さいころ':
       case 'サイコロ':
-        text =  String(dice[Math.floor(Math.random() * 5 )]);
+      case 'dice':
+        text =  String(dice[Math.floor(Math.random() * 5)]);
+        break;
+      case 'ご注文は?':
+        text = 'うさぎですか?';
+        break;
+      case 'π':
+        text = '3.14159265358979323846264338...';
+        break;
+      case '真実は':
+        text = 'いつも一つ！';
+        break;
+      case 'さあゲームを始めよう':
+        text = 'アッシェンテ!';
+        break;
+      case 'カレーライス':
+        text = '僕は悪くない！';
+        break;
+      case 'お願い':
+        text = 'どんぴしゃり、お願いが叶った。';
+        break;
+      case 'めがね':
+      case 'メガネ':
+        text = 'めがね、めがねぇ～';
+        break;
+      case '脳':
+        text = 'あぁ脳が震える震える震える震えるぅぅぅぅぅ！';
+        break;
+      case '紹介':
+        text = 'ワタシは魔女教、大罪司教『怠惰』担当、ペテルギウス・ロマネコンティ……デス！';
         break;
       default:
-        var index = post.text.indexOf('した.');
-        if (index != -1) {
+        var index = -1;
+        if ((index = post.text.indexOf('した.')) != -1) {
           text = post.text.slice(0, index) + 'したなんてすごいですね!!';
+        } else if ((index = post.text.indexOf('ゲーム')) != -1){
+          text = 'これは、ゲームであっても遊びではない';
+        } else if ((index = post.text.indexOf('逃')) != -1){
+          text = '逃げちゃダメだ。逃げちゃダメだ。逃げちゃダメだ。';
+        } else if ((index = post.text.indexOf('疲')) != -1){
+          text = 'アナタ怠惰デスね？';
         } else {
-          doResponse = false;
+          text = 'あんたバカァ？';
         }
         break;
     }
